b8d8a1573a50cb85d59f9c47c72bdb2add068509
rakibriaz <alriazgwd.com>
2023-08-17 22:14:47 +0600

    first commit

---
+++ js/price.js
@@ -0,0 +1,11 @@
+function getInputFieldById(inputId){
+    const inputField = document.getElementById(inputId);
+    const inputFieldValueString = inputField.value;
+    return inputFieldValueString;
+}
+
+
+
+document.getElementById("submit-btn").addEventListener("click", function(){
+    
+})
